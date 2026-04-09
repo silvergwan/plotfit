e0475441425ad874b93316a257beee9709c0dd73
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-09 10:11:05 +0900

    feat: 기본적인 UI 구현 및 useState대한 학습 내용 기입(주석으로)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+// next에서 이걸 쓰지 않으면 서버컴포넌트로 인식을 하여 브라우저 이벤트를 사용할 수가 없음
+
+import { useState } from "react";
+// useState는 사용자가 값을 입력하면 그 내용을 api로 전달하기 전에 가지고 있어야 보낼 수 있는데 그 저장소 역할을 함
+
 export default function Home() {
+  const [baseProfile, setBaseProfile] = useState("");
+  // baseProfile은 상태이고, setBaseProfile은 상태 변경 함수임
+  // 상태를 단순 변수로 이해하면 안됨, 상태는 React가 값의 변경을 알아채고 리렌더링까지 하는 아주 고차원적 개념
+  const [plotContent, setPlotContent] = useState("");
+
   return (
     <main>
       <h1>PlotFit</h1>
+      <p>플롯에 맞는 나만의 프로필을 만들어드립니다.</p>
+
+      <textarea
+        placeholder="기본 프로필을 입력하세요"
+        value={baseProfile}
+        onChange={(e) => setBaseProfile(e.target.value)}
+        // 키를 하나 누를 때마다 onChange가 실행되고 e.target.value로 그 시점의 데이터를 setBaseProfile(useState) 저장소에 넣음
+        // 그리고 setBaseProfile가 실행되면 React가 자동으로 화면을 다시 그림 -> React의 핵심! = 값이 바뀌면 알아서 렌더링
+      />
+      <textarea
+        placeholder="플롯 내용을 붙여넣으세요"
+        value={plotContent}
+        onChange={(e) => setPlotContent(e.target.value)}
+        // 같은 내용
+      />
+
+      <button>프로필 생성하기</button>
+
+      <p>{baseProfile}</p>
+      <p>{plotContent}</p>
     </main>
   );
 }
