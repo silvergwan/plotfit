022aa0de2fe730baaf057bea70e71826f59f336b
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-12 14:32:25 +0900

    update: Button 단위 최적화( h-[52px] -> h-13)

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ onClick, label }: ButtonProps) {
     <div className="px-4 mt-4">
       <button
         onClick={onClick}
-        className="w-full h-[52px] rounded-xl bg-[#6728FF] text-white font-medium text-[15px] p-3"
+        className="w-full h-13 rounded-xl bg-[#6728FF] text-white font-medium text-[15px] p-3"
       >
         {label}
       </button>
