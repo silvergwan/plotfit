7f0397c571c2940e9a0b7da988d9acf4a9797f90
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-12 14:28:48 +0900

    feat: Header 컴포넌트 개발

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="mx-2.5 h-13 flex items-center">
+      <span className="px-1.5 text-[22px] font-medium tracking-tight">
+        플롯핏
+      </span>
+    </header>
+  );
+}
