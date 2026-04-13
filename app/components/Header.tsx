5b8c135c8862ebe13ea6e15264be4fea1d9cf7ed
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-13 11:13:22 +0900

    update: header 단위 최적화

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="mx-2.5 h-[52px] flex items-center border-b-[0.5px] border-white/20">
+    <header className="mx-2.5 h-13 flex items-center border-b-[0.5px] border-white/20">
       <span className="px-1.5 text-[22px] font-medium tracking-tight">
         Plotfit
       </span>
