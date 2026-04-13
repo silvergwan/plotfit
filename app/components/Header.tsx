a1892880a221c8648a28636834c7df1d74880428
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-13 11:13:01 +0900

    update: border 바텀 추가와 헤더 타이틀 변경

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <header className="mx-2.5 h-13 flex items-center">
+    <header className="mx-2.5 h-[52px] flex items-center border-b-[0.5px] border-white/20">
       <span className="px-1.5 text-[22px] font-medium tracking-tight">
-        플롯핏
+        Plotfit
       </span>
     </header>
   );
