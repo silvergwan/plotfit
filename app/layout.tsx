aef2e0aa36750005075fdbc9f6120bb51f198e81
EUNGWANCHOI <silvergwan@gmail.com>
2026-04-13 12:38:05 +0900

    update: add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "PlotFit",
+  description: "플롯에 맞는 나만의 대화 프로필을 만들어드립니다.",
+  openGraph: {
+    title: "PlotFit",
+    description: "Zeta 유저를 위한 플롯 맞춤형 프로필 생성 서비스",
+    url: "https://silvergwan.site",
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
